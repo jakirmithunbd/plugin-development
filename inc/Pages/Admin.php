e3ab3943c558997f54f9b78de2b7d36d4485d733
Md Jakirul Islam <jakirmithunbd@gmail.com>
2023-03-16 13:00:22 +0600

    lesson 16

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -1,22 +1,81 @@
-<?php
+<?php 
 /**
  * @package  AlecadddPlugin
-*/
+ */
 namespace Inc\Pages;
 
+use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
+use Inc\Api\Callbacks\AdminCallbacks;
 
+/**
+* 
+*/
 class Admin extends BaseController
 {
-    public function register() {
-        add_action( 'admin_menu', [$this, 'add_admin_pages'] );
-    }
+	public $settings;
 
-    public function add_admin_pages() {
-        add_menu_page('Alecade Plugin', 'Alecade', 'manage_options', 'alecade_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
-    }
+	public $callbacks;
 
-    public function admin_index() {
-        require_once $this->plugin_path . '/templates/admin.php';
-    }
+	public $pages = array();
+
+	public $subpages = array();
+
+	public function register() 
+	{
+		$this->settings = new SettingsApi();
+
+		$this->callbacks = new AdminCallbacks();
+
+		$this->setPages();
+
+		$this->setSubpages();
+
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+	}
+
+	public function setPages() 
+	{
+		$this->pages = array(
+			array(
+				'page_title' => 'Alecaddd Plugin', 
+				'menu_title' => 'Alecaddd', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_plugin', 
+				'callback' => array( $this->callbacks, 'adminDashboard' ), 
+				'icon_url' => 'dashicons-store', 
+				'position' => 110
+			)
+		);
+	}
+
+	public function setSubpages()
+	{
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'alecaddd_plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_cpt', 
+				'callback' => array( $this->callbacks, 'adminCpt' )
+			),
+			array(
+				'parent_slug' => 'alecaddd_plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_taxonomies', 
+				'callback' => array( $this->callbacks, 'adminTaxonomy' )
+			),
+			array(
+				'parent_slug' => 'alecaddd_plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_widgets', 
+				'callback' => array( $this->callbacks, 'adminWidget' )
+			)
+		);
+	}
 }
