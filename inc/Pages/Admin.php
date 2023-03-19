ca1cc49e3fea529962599b97f234e694351865dd
Md Jakirul Islam <jakirmithunbd@gmail.com>
2023-03-19 16:10:15 +0600

    before adding page content

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -31,10 +31,6 @@ class Admin extends BaseController
 
 		$this->setSubpages();
 
-		$this->setSettings();
-		$this->setSections();
-		$this->setFields();
-
 		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 
@@ -81,66 +77,5 @@ class Admin extends BaseController
 				'callback' => array( $this->callbacks, 'adminWidget' )
 			)
 		);
-	}
-
-	public function setSettings()
-	{
-		$args = array(
-			array(
-				'option_group' => 'alecaddd_options_group',
-				'option_name' => 'text_example',
-				'callback' => array( $this->callbacks, 'alecadddOptionsGroup' )
-			),
-			array(
-				'option_group' => 'alecaddd_options_group',
-				'option_name' => 'first_name'
-			)
-		);
-
-		$this->settings->setSettings( $args );
-	}
-
-	public function setSections()
-	{
-		$args = array(
-			array(
-				'id' => 'alecaddd_admin_index',
-				'title' => 'Settings',
-				'callback' => array( $this->callbacks, 'alecadddAdminSection' ),
-				'page' => 'alecaddd_plugin'
-			)
-		);
-
-		$this->settings->setSections( $args );
-	}
-
-	public function setFields()
-	{
-		$args = array(
-			array(
-				'id' => 'text_example',
-				'title' => 'Text Example',
-				'callback' => array( $this->callbacks, 'alecadddTextExample' ),
-				'page' => 'alecaddd_plugin',
-				'section' => 'alecaddd_admin_index',
-				'args' => array(
-					'label_for' => 'text_example',
-					'class' => 'example-class'
-				)
-			),
-			array(
-				'id' => 'first_name',
-				'title' => 'First Name',
-				'callback' => array( $this->callbacks, 'alecadddFirstName' ),
-				'page' => 'alecaddd_plugin',
-				'section' => 'alecaddd_admin_index',
-				'args' => array(
-					'label_for' => 'first_name',
-					'class' => 'example-class'
-				)
-			)
-		);
-
-		$this->settings->setFields( $args );
 	}
 }
